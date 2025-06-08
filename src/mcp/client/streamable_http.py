ef23b6b976c3b0101e241acf3dd9102e68832bcf
Ernesto Cullari <ernesto@cullarimedia.com>
2025-06-08 04:09:34 +0000

    Add stub for streamable_http

---
+++ src/mcp/client/streamable_http.py
@@ -0,0 +1,7 @@
+def streamablehttp_client(*args, **kwargs):
+    raise NotImplementedError("Stub: streamablehttp_client")
+
+
+class GetSessionIdCallback:
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError("Stub: GetSessionIdCallback")
